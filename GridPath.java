9c5e6ff0eee86e5f785a96d87412f3ae3e6cacbb
joannac112 <joannac112@nycstudents.net>
2024-11-12 14:32:58 +0000

    dd

--- GridPath.java
+++ GridPath.java
@@ -1,24 +1,24 @@
 public class GridPath {
-    public GridPath(int[][] values)
+    /** Initialized in the constructor with distinct values that never change */
+    private int[][] grid;
+    public Gridpath(int[][] values) 
     {
         grid = values;
     }
-    public String toString()
-    {
+    public String toString() {
         String s = "";
-        for (int row = 0; row < grid.length; row++)
-        { 
-            for (int col = 0; col < grid[0].length; col++)
-            {
-                s += grid[row][col] + " ";
+        for (int[] row : grid) {
+            for (int col : row) {
+                s += col + " ";
             }
             s += "/n";
         }
         return s;
     }
-    /** Initialized in the constructor with distinct values that never change */
-    private int[][] grid;
-
+    public String toString()
+    {
+        return theRow + ", " + theCol;
+    }
     /**
      * Returns the Location representing a neighbor of the grid element at row and
      * col,
@@ -29,12 +29,13 @@ public class GridPath {
      * grid.
      */
     public Location getNextLoc(int row, int col) {
-        int below = grid[row + 1][col];
-        int right = grid[row][col + 1];
-        if (below<right) return new Location(row + 1, col);
-        else return new Location(row, col + 1);
-    }
-        /* to be implemented in part (a) */
+        int below = Integer.MAX_VALUE;
+        if(row < grid.length - 1) below = grid[row + 1][col];
+        int right = Integer.MAX_VALUE;
+        if(col < grid[0].length - 1) right = grid[row][col + 1];
+        if(below < right) return new Location(row + 1, col);
+        else return new Location (row, col + 1);
+        /* to be implemented in part (a) */ }
 
     /**
      * Computes and returns the sum of all values on a path through grid, as
@@ -47,6 +48,5 @@ public class GridPath {
      */
     public int sumPath(int row, int col) {
         return 0;
-    }
-        /* to be implemented in part (b) */
+        /* to be implemented in part (b) */ }
 }
